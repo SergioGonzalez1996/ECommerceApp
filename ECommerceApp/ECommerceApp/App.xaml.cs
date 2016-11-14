bcb90a40a7d767164c2743c634a878f00d110f0e
Sergio <Sergio@SERGIO>
2016-11-14 14:33:21 -0500

    Navigation in menu (until video 103)

---
+++ ECommerceApp/ECommerceApp/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using ECommerceApp.Pages;
+using Xamarin.Forms;
+
+namespace ECommerceApp
+{
+    public partial class App : Application
+    {
+        #region Properties
+        public static NavigationPage Navigator { get; internal set; }
+        public static MasterPage Master { get; internal set; }
+        #endregion
+
+        #region Constructor
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MasterPage();
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
+}
---
+++ ECommerceApp/ECommerceApp/Services/NavigationService.cs
@@ -0,0 +1,49 @@
+﻿using ECommerceApp.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Services
+{
+    public class NavigationService
+    {
+        public async Task Navigate (string pageName)
+        {
+            App.Master.IsPresented = false;
+
+            switch (pageName)
+            {
+                case "ProductsPage":
+                    await App.Navigator.PushAsync(new ProductsPage());
+                    break;
+                case "CustomersPage":
+                    await App.Navigator.PushAsync(new CustomersPage());
+                    break;
+                case "OrdersPage":
+                    await App.Navigator.PushAsync(new OrdersPage());
+                    break;
+                case "DeliveriesPage":
+                    await App.Navigator.PushAsync(new DeliveriesPage());
+                    break;
+                case "SyncPage":
+                    await App.Navigator.PushAsync(new SyncPage());
+                    break;
+                case "SetupPage":
+                    await App.Navigator.PushAsync(new SetupPage());
+                    break;
+
+                case "LogoutPage":
+                    await App.Navigator.PopToRootAsync();
+                    break;
+                case "UserPage":
+                    await App.Navigator.PopToRootAsync();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
